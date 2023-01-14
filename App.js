2c9c65ff0efc6f1c658482809f3c6e48dbceea3a
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2023-01-14 16:46:09 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function INST() {
+  alert("Welcome to the web page about Vistadome Coach in India. Hope You Enjoy It!"); 
+  alert("Instructions - In Speciality and Images sceton, hover over the images if you are using laptop and tap on the images if you are using mobile. Don't forget to give feedback if you like it.");
+}
+
+function LOAD() {
+  alert("Loading...");
+}
